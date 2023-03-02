d3dc98d019b4b1c15c524449eff62040176c0b9c
fengliushijie <1099216627@qq.com>
2023-03-02 11:25:57 +0800

    feat: 更新部署配置

--- src/main.ts
+++ src/main.ts
@@ -5,27 +5,27 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { AllExceptionFilter } from './filters/all-exception.filter';
 import rateLimit from 'express-rate-limit';
 import helmet from 'helmet';
-async function bootstrap() {  
+async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: true,//允许跨域
-      credentials: true,//允许携带cookie
-    }
+      origin: true, //允许跨域
+      credentials: true, //允许携带cookie
+    },
   });
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app.setGlobalPrefix('api/v1');
-  
+
   //filter
   const httpAdapter = app.get(HttpAdapterHost); //获取http适配器
   app.useGlobalFilters(new AllExceptionFilter(new Logger(), httpAdapter));
   //pipe
   app.useGlobalPipes(new ValidationPipe());
-  rateLimit
+  rateLimit;
   app.use(
     rateLimit({
       windowMs: 15 * 60 * 1000, // 15 minutes
       max: 1000,
-    })
+    }),
   );
   //helmet头部安全，防止xss攻击
   app.use(helmet());
